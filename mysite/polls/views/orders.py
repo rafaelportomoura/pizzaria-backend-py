6a964cec0b872653552851f424167fbf8e82526e
Luiz Montuani <luizmontuani@ioasys.com.br>
2023-06-06 01:41:44 -0300

    add login required and table

--- mysite/polls/views/orders.py
+++ mysite/polls/views/orders.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from polls.controller.orders import OrderService
 import polls.views.templates as templates
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def all(request):
     orders = OrderService().all()
     context = {
